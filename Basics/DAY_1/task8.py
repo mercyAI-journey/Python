a57bc5802aa0e86db2ebb160f7c53085674f3256
MercyXp <munzenzimercy2024@gmail.com>
2026-01-28 15:05:29 +0200

    Task 8 complete | understanding the different types of pyhton operators

---
+++ Basics/DAY_1/task8.py
@@ -0,0 +1,11 @@
+a = 15
+b = 4
+
+sum = a + b
+product = a * b
+difference = a - b
+integer_division = a // b
+division = a / b
+modulo = a % b
+
+print(sum, " ", product , " ", difference, " ", integer_division, " ", division, " ", modulo)
